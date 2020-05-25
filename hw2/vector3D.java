a15ff9e93258e8bcd924a5c4249c5a374cdc67af
Wei Nien Chen <65255569+chenwein@users.noreply.github.com>
2020-05-25 01:38:49 -0700

    add HW files vector and unit test

--- hw2/vector3D.java
+++ hw2/vector3D.java
@@ -1,1 +1,149 @@
+package vector;
 
+class vector3D {
+    //fields
+    private double x;
+    private double y;
+    private double z;
+
+    /**
+     * default constructor
+     * @param x double component of vector
+     * @param y ''
+     * @param z ''
+     */
+    public vector3D(double x, double y, double z) {
+        setX(x);
+        setY(y);
+        setZ(z);
+    }
+
+    /**
+     * no argument constructor
+     */
+    public vector3D(){
+
+    }
+
+    /**
+     * copy constrcutor
+     * @param other an object of vector3D class
+     */
+    public vector3D(vector3D other) {
+        this(other.getX(), other.getY(), other.getZ());
+    }
+
+    //get and set
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    //methods
+    /**
+     *  to string
+     * @return (x,y,z) round to 2 decimal places
+     */
+    @Override
+    public String toString() {
+        if (getX() + getY() + getZ() == 0) {
+            return "";
+        }
+        return String.format("(%.2f, %.2f, %.2f)", getX(), getY(), getZ());
+    }
+
+    /**
+     * get magnitude method
+     * @return double
+     * @throws IllegalArgumentException if magnitude is less than 0
+     */
+    public double getMagnitude() {
+         double magnitude =  Math.sqrt(Math.pow(this.getX(), 2) + Math.pow(this.getY(), 2) + Math.pow(this.getZ(), 2));
+         return magnitude;
+    }
+
+    /**
+     * normalize vector
+     * @return a new vector object with normalized components
+     * @throws IllegalStateException if vector 0
+     */
+    public vector3D normalize() throws IllegalStateException {
+        double magnitude = this.getMagnitude();
+        if (getX() + getY() + getZ() == 0) {
+            throw new IllegalStateException("normalize zero vector");
+        }
+        return new vector3D(getX() / magnitude,getY() / magnitude, getZ() / magnitude );
+    }
+
+    /**
+     * equals method
+     * @param o an object
+     * @return true if the object values are equals
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof vector3D)) return false;
+        vector3D vector3D = (vector3D) o;
+        return Double.compare(vector3D.getX(), getX()) == 0 &&
+                Double.compare(vector3D.getY(), getY()) == 0 &&
+                Double.compare(vector3D.getZ(), getZ()) == 0;
+    }
+
+    /**
+     * adding two vectors together
+     * @param other a vector object
+     * @return a new vector with the added component values
+     */
+    public vector3D add(vector3D other) {
+        return new vector3D(getX() + other.getX(), getY() + other.getY(), getZ() + other.getZ());
+    }
+
+    /**
+     * multiply each vector component by constant
+     * @param constant double
+     * @return new vector with scaled components
+     */
+    public vector3D multiply(double constant) {
+        return new vector3D(getX() * constant, getY() * constant, getZ() * constant);
+    }
+
+    /**
+     * dotproduct of 2 vectors a1*b1 + a2*b2
+     * @param other vector3D object
+     * @return new vector3D object
+     */
+    public double dotProduct(vector3D other) {
+        return (getX() * other.getX()) + (getY() * other.getY()) + (getZ() * other.getZ());
+
+    }
+
+    public double angleBetween(vector3D other) throws IllegalStateException {
+        if (this.getX() + this.getY() + this.getY() == 0 ||
+        other.getX() + other.getY() + other.getZ() == 0) {
+            throw new IllegalStateException("vector cannot be 0");
+        }
+        double radian = Math.acos( (dotProduct(other)) / (this.getMagnitude() * other.getMagnitude()) );
+        return Math.toDegrees(radian);
+    }
+
+}
--- hw2/vector3DTest.java
+++ hw2/vector3DTest.java
@@ -1,1 +1,95 @@
+package vector;
 
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.junit.Assert.*;
+
+public class vector3DTest {
+
+
+    //test default constructor
+    @Test
+    public void testConstructor() {
+        vector3D testVector = new vector3D(10.5, 3, 6);
+        //expected, actual, delta precision loss
+        //for calculated numbers 5.1 + 0.1 = 5.2, but actual output is 5.19999..
+        //delta can be 0.001 or less
+        assertEquals(10.5, testVector.getX(), 0);
+        assertEquals(3, testVector.getY(), 0);
+        assertEquals(6, testVector.getZ(), 0);
+    }
+
+    //test empty vector to string
+    @Test
+    public void toStringEmptyTest() {
+        vector3D testVector = new vector3D();
+        assertEquals(testVector.toString(), "");
+    }
+
+    @Test
+    public void toStringTest() {
+        vector3D testVector = new vector3D(1.572, 4.336, 6.5);
+        testVector.toString();
+        assertEquals("(1.57, 4.34, 6.50)", testVector.toString());
+    }
+
+    //no exceptions expected
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void getMagnitudeTest() {
+        //how to implement exception test?
+        vector3D testVector = new vector3D(3.5, -2.5, 6);
+        assertEquals(Math.sqrt(54.5), testVector.getMagnitude(), 0.1);
+    }
+
+    @Test
+    public void testNormalizeException() {
+        exception.expect(IllegalStateException.class);
+        vector3D testVector = new vector3D(0,0,0);
+        testVector.normalize();
+    }
+
+    @Test
+    public void testNormalize() {
+        vector3D testVector1 = new vector3D(0.6,0,0.8);
+        vector3D testVector2 = new vector3D(9,0,12);
+        vector3D testVector3 = testVector2.normalize();
+        assertEquals(testVector1, testVector3);
+    }
+
+    @Test
+    public void testAdd() {
+        vector3D testVector1 = new vector3D(6,-2,7);
+        vector3D testVector2 = new vector3D(-4,4,1);
+        vector3D testVector3 = new vector3D(2, 2, 8);
+        assertEquals(testVector3, testVector1.add(testVector2));
+    }
+
+    @Test
+    public void testMultiply() {
+        double constant = 3;
+        vector3D testVector1 = new vector3D(6,-2,3);
+        vector3D testVector3 = new vector3D(18, -6, 9);
+        assertEquals(testVector3, testVector1.multiply(constant));
+    }
+
+    @Test
+    public void testDotProduct() {
+        vector3D testVector1 = new vector3D(1,2,3);
+        vector3D testVector3 = new vector3D(-2, 0, 5);
+        assertEquals(13, testVector1.dotProduct(testVector3), 0.0001);
+    }
+
+    @Test
+    public void testAngleBetween() {
+        vector3D testVector1 = new vector3D(4,3,0);
+        vector3D testVector3 = new vector3D(3, 5, 0);
+        assertEquals(22.16634582, testVector1.angleBetween(testVector3), 0.00000001);
+    }
+
+
+}
