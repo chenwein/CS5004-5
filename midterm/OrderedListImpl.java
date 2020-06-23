6c3d1a81d4b62be119c2580ffe80114e23596adb
Wei Nien Chen <65255569+chenwein@users.noreply.github.com>
2020-06-22 20:00:13 -0700

    midterm java files first submission

---
+++ midterm/OrderedListImpl.java
@@ -0,0 +1,171 @@
+package Midterm;
+
+public class OrderedListImpl implements OrderedList{
+    private ListNode head;
+    private int maxSize;
+    private int size;
+    public OrderedListImpl(int maxSize, ListNode head) {
+        this.maxSize = maxSize;
+        this.head = head;
+        size = 0;
+    }
+    public OrderedListImpl(ListNode head) {
+        this.head = head;
+        size = 0;
+    }
+
+    @Override
+    public int getMin() throws IllegalStateException {
+        if(head == null) {
+            throw new IllegalStateException("empty");
+        }
+        else {
+            return head.value;
+        }
+    }
+
+    @Override
+    public int getMax() throws IllegalStateException {
+        ListNode traversePointer = head;
+        if(traversePointer == null) {
+            throw new IllegalStateException("empty");
+        }
+
+        while (traversePointer.next != null) {
+            traversePointer = traversePointer.next;
+        }
+        return traversePointer.value;
+    }
+
+    @Override
+    public double getMedian() throws IllegalStateException {
+        ListNode traversePointer = head;
+        if(traversePointer == null) {
+            throw new IllegalStateException("empty");
+        }
+        else  {
+            int mid = this.size()/2;
+            while(mid > 0) {
+                traversePointer = traversePointer.next;
+                mid--;
+            }
+            if (this.size()%2 == 1) {
+                return (double) traversePointer.value;
+            }
+            else {
+                return (traversePointer.value + traversePointer.next.value) / 2.0;
+            }
+        }
+    }
+
+    @Override
+    public void add(int val) {
+        if(this.getMax() < val) {
+            ListNode newNode = new ListNode(val);
+            ListNode dummy = new ListNode(-1);
+            dummy.next = head;
+            ListNode prev = dummy;
+            ListNode traversePointer = head;
+            while (traversePointer != null && val > traversePointer.value) {
+                traversePointer = traversePointer.next;
+                prev = prev.next;
+            }
+            prev.next = newNode;
+            size++;
+        }
+        else {
+            return;
+        }
+    }
+
+    @Override
+    public OrderedListImpl merge(OrderedListImpl other) {
+        ListNode one = this.head;
+        ListNode two = other.head;
+        ListNode dummyHead = new ListNode(-1);
+        ListNode newNode = dummyHead;
+        while (one != null && two != null) {
+            if (one.value <= two.value) {
+                newNode.next = one;
+                one = one.next;
+            } else { //one > two
+                newNode.next = two;
+                two = two.next;
+            }
+            newNode = newNode.next;
+        }
+        //post processing, add remaining nodes
+        //L1 = null, L2 = 1 -> 2 -> null, merge L1 and L2 to 1 -> 2 -> null. also takes care of this.
+        if (one == null ) {
+            newNode.next = two;
+        } else { //two == null
+            newNode.next = one;
+        }
+        return new OrderedListImpl(dummyHead.next);
+    }
+
+
+    @Override
+    public void remove(int val) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode prev = dummy;
+        ListNode traversePointer = head;
+        while (traversePointer != null && val != traversePointer.value) {
+            traversePointer = traversePointer.next;
+            prev = prev.next;
+        }
+        if(traversePointer == null) {
+            return;
+        }
+        else {
+            prev.next = traversePointer.next;
+        }
+        size--;
+    }
+
+    @Override
+    public void removeAll(int v) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode prev = dummy;
+        ListNode traversePointer = head;
+        while (traversePointer != null && v != traversePointer.value) {
+            traversePointer = traversePointer.next;
+            prev = prev.next;
+        }
+        if(traversePointer == null) {
+            return;
+        }
+        else {
+            prev.next = null;
+        }
+        ListNode newTraverse = head;
+        int counter = 1;
+        while (newTraverse != null) {
+            newTraverse = newTraverse.next;
+            counter++;
+        }
+        this.size = counter;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        if(this.size() == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    @Override
+    public OrderedListImpl intersection(OrderedListImpl other) {
+        return other; //no time
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+}
