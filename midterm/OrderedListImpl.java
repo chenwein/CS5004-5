d574f9fedd7af7774f3cb370b8b48fff3e747bf1
Wei Nien Chen <65255569+chenwein@users.noreply.github.com>
2020-06-22 21:41:21 -0700

    I realized I put the Implementation code in the interface code.
    Switched it back to interface

--- midterm/OrderedListImpl.java
+++ midterm/OrderedListImpl.java
@@ -1,171 +1,50 @@
 package Midterm;
 
-public class OrderedListImpl implements OrderedList{
-    private ListNode head;
-    private int maxSize;
-    private int size;
-    public OrderedListImpl(int maxSize, ListNode head) {
-        this.maxSize = maxSize;
-        this.head = head;
-        size = 0;
-    }
-    public OrderedListImpl(ListNode head) {
-        this.head = head;
-        size = 0;
+import java.util.LinkedList;
+
+public interface OrderedList {
+    default int getMin() throws IllegalStateException {
+        return 0;
     }
 
-    @Override
-    public int getMin() throws IllegalStateException {
-        if(head == null) {
-            throw new IllegalStateException("empty");
-        }
-        else {
-            return head.value;
-        }
+    default int getMax() throws IllegalStateException {
+        return 0;
     }
 
-    @Override
-    public int getMax() throws IllegalStateException {
-        ListNode traversePointer = head;
-        if(traversePointer == null) {
-            throw new IllegalStateException("empty");
-        }
-
-        while (traversePointer.next != null) {
-            traversePointer = traversePointer.next;
-        }
-        return traversePointer.value;
+    default double getMedian() throws IllegalStateException {
+        return 0;
     }
 
-    @Override
-    public double getMedian() throws IllegalStateException {
-        ListNode traversePointer = head;
-        if(traversePointer == null) {
-            throw new IllegalStateException("empty");
-        }
-        else  {
-            int mid = this.size()/2;
-            while(mid > 0) {
-                traversePointer = traversePointer.next;
-                mid--;
-            }
-            if (this.size()%2 == 1) {
-                return (double) traversePointer.value;
-            }
-            else {
-                return (traversePointer.value + traversePointer.next.value) / 2.0;
-            }
-        }
+    default void add(int val) {
+        return;
     }
 
-    @Override
-    public void add(int val) {
-        if(this.getMax() < val) {
-            ListNode newNode = new ListNode(val);
-            ListNode dummy = new ListNode(-1);
-            dummy.next = head;
-            ListNode prev = dummy;
-            ListNode traversePointer = head;
-            while (traversePointer != null && val > traversePointer.value) {
-                traversePointer = traversePointer.next;
-                prev = prev.next;
-            }
-            prev.next = newNode;
-            size++;
-        }
-        else {
-            return;
-        }
+    default OrderedListImpl merge(OrderedListImpl other) {
+        return null;
     }
 
-    @Override
-    public OrderedListImpl merge(OrderedListImpl other) {
-        ListNode one = this.head;
-        ListNode two = other.head;
-        ListNode dummyHead = new ListNode(-1);
-        ListNode newNode = dummyHead;
-        while (one != null && two != null) {
-            if (one.value <= two.value) {
-                newNode.next = one;
-                one = one.next;
-            } else { //one > two
-                newNode.next = two;
-                two = two.next;
-            }
-            newNode = newNode.next;
-        }
-        //post processing, add remaining nodes
-        //L1 = null, L2 = 1 -> 2 -> null, merge L1 and L2 to 1 -> 2 -> null. also takes care of this.
-        if (one == null ) {
-            newNode.next = two;
-        } else { //two == null
-            newNode.next = one;
-        }
-        return new OrderedListImpl(dummyHead.next);
+    default void remove(int val) {
+        return;
     }
 
-
-    @Override
-    public void remove(int val) {
-        ListNode dummy = new ListNode(-1);
-        dummy.next = head;
-        ListNode prev = dummy;
-        ListNode traversePointer = head;
-        while (traversePointer != null && val != traversePointer.value) {
-            traversePointer = traversePointer.next;
-            prev = prev.next;
-        }
-        if(traversePointer == null) {
-            return;
-        }
-        else {
-            prev.next = traversePointer.next;
-        }
-        size--;
+    default void removeAll(int v) {
+        return;
     }
 
-    @Override
-    public void removeAll(int v) {
-        ListNode dummy = new ListNode(-1);
-        dummy.next = head;
-        ListNode prev = dummy;
-        ListNode traversePointer = head;
-        while (traversePointer != null && v != traversePointer.value) {
-            traversePointer = traversePointer.next;
-            prev = prev.next;
-        }
-        if(traversePointer == null) {
-            return;
-        }
-        else {
-            prev.next = null;
-        }
-        ListNode newTraverse = head;
-        int counter = 1;
-        while (newTraverse != null) {
-            newTraverse = newTraverse.next;
-            counter++;
-        }
-        this.size = counter;
+    default boolean isEmpty() {
+        return false;
     }
 
-    @Override
-    public boolean isEmpty() {
-        if(this.size() == 0) {
-            return true;
-        }
-        else {
-            return false;
-        }
+    default OrderedListImpl intersection(OrderedListImpl other) {
+        return null;
     }
 
-    @Override
-    public OrderedListImpl intersection(OrderedListImpl other) {
-        return other; //no time
-    }
+    // It gets complicated to override a static method defined in interface.
+    // So I have sort() commented out. But you still need to implement it in your class,
+    // with the same method signature.
+    // static OrderedList sort(LinkedList<Integer> other);
 
-    @Override
-    public int size() {
-        return size;
+    default int size() {
+        return 0;
     }
 }
